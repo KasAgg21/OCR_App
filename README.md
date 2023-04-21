748afb7d9faa633095180a34b9d0ec88ac86770e
aggkas21 <aggkas21@gmail.com>
2023-04-21 21:59:17 +0530

    Day 2

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # ocr_app
 
-A OCR App
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+It uses Firebase ML Vision API to extract text from images and it can convert images from camera or gallery  to pdf documents.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:html';
+import 'dart:io';
+import 'dart:js';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:ocr_app/result_screen.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -26,10 +34,14 @@ class MainScreen extends StatefulWidget{
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>{
-  bool isPermission = false;
+class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
+  bool _isPermission = false;
 
   late final Future<void> _future;
+
+  CameraController? _cameraController;
+
+  final _textRecognizer = TextRecognizer();
 
   @override
   void initState(){
@@ -38,32 +50,152 @@ class _MainScreenState extends State<MainScreen>{
     _future = _requestCp();
   }
 
+  @override
+  void dispose(){
+    WidgetsBinding.instance.removeObserver(this);
+    _stopcamera();
+    _textRecognizer.close();
+    super.dispose();
+  }
+
+  void didChangeAppLifeCycle(AppLifecycleState state)
+  {
+    //Control Camera Flow
+    if(_cameraController==null || !_cameraController!.value.isInitialized)
+      {
+        return;
+      }
+    if(state==AppLifecycleState.inactive){
+      _stopcamera();
+    }else if (state == AppLifecycleState.resumed && _cameraController!=null && _cameraController!.value.isInitialized){
+      _startCamera();
+    }
+  }
+
   Widget build(BuildContext context)
   {
     return FutureBuilder(future: _future,
     builder: (context, snapshot){
-      return Scaffold(
+      return Stack(
+        children: [
+          if(_isPermission)
+            FutureBuilder<List<CameraDescription>>(
+              future: availableCameras(),
+              builder: (context,snapshot){
+                if(snapshot.hasData){
+                  _initCameraController(snapshot.data!);
+                  return Center(child: CameraPreview(_cameraController!),);
+                }else{
+                  return const LinearProgressIndicator();
+                }
+              },
+            ),
+      Scaffold(
         appBar: AppBar(
           title: const Text('OCR App'),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.only(left: 24, right: 24),
-            child: Text(
-              isPermission
-                ? 'Camera Granted'
-                : 'Not Granted',
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
+        backgroundColor: _isPermission ? Colors.transparent : null,
+        body: _isPermission
+            ? Column(
+      children: [
+              Expanded(
+                  child: Container()
+            ,),
+      Container(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: const Center(
+      child: ElevatedButton(
+          onPressed: _scanImage,
+          child: Text('Scan Text')),
+      ),
+      )
+      ],
+      )
+            : Center(
+                  child: Container(
+                  padding: const EdgeInsets.only(left: 24, right: 24),
+                        child: const Text(
+                              'Not Granted',
+                              textAlign: TextAlign.center,
+                                          ),
+                                  ),
+                  ),
+            )
+        ],
       );
     },);
   }
 
   Future<void> _requestCp() async{
     final status = await Permission.camera.request();
-    isPermission = status == PermissionStatus.granted;
+    _isPermission = status == PermissionStatus.granted;
   }
 
+  void _startCamera()
+  {
+    if(_cameraController !=null)
+      {
+        _cameraSelected(_cameraController!.description);
+      }
+  }
+
+  void _stopcamera(){
+    if(_cameraController!=null){
+      _cameraController?.dispose();
+    }
+  }
+
+  void _initCameraController(List<CameraDescription> cameras)
+  {
+    if (_cameraController !=null)
+      {
+        return;
+      }
+    CameraDescription? camera;
+    for (var i = 0;i<cameras.length;i++)
+      {
+        final CameraDescription current = cameras[i];
+        if(current.lensDirection==CameraLensDirection.back)
+          {
+            camera=current;
+            break;
+          }
+      }
+    if(camera !=null)
+      {
+        _cameraSelected(camera);
+      }
+  }
+
+  Future<void> _cameraSelected(CameraDescription camera) async
+  {
+    _cameraController = CameraController(camera, ResolutionPreset.max,enableAudio: false);
+
+    await _cameraController?.initialize();
+
+    if(!mounted)
+      {
+        return;
+      }
+    setState(() {});
+  }
+
+  Future<void> _scanImage() async{
+    if(_cameraController == null) {return;}
+
+    final navigator = Navigator.of(context as BuildContext);
+
+    try{
+      final pictureFile = await _cameraController!.takePicture();
+      final file = File(pictureFile.path);
+      final inputImage = InputImage.fromFile(file);
+      final recoginizedText = await _textRecognizer.processImage(inputImage);
+      await navigator.push(MaterialPageRoute(
+          builder: (context)=>ResultScreen(text: recoginizedText.text)));
+    }
+    catch(e)
+    {
+      ScaffoldMessenger.of(context as BuildContext).showSnackBar(const SnackBar(content: Text('An Error Occured while Scanning')));
+    }
+  }
 }
